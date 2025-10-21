8bbc055f405499ccb5c0f37433a806fbaeb1753e
wonswe <64626054+wonswe@users.noreply.github.com>
2025-10-20 21:02:18 -0400

    primitive types

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-let a = 5;
-console.log(a);
+// number, string, boolean, null, undefined
+// 위 primitive type을 제외한 모든 것이 오브젝트이다
+let number = 2;
+let number2 = number;
+console.log(number);
+console.log(number2);
+
+number2 = 3;
+
+console.log(number);
+console.log(number2);
